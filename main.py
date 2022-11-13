925d32c66e7046bd2f45eae55853e0fd214ba48e
Bryan Wong <bryanw0ng@ucla.edu>
2022-11-12 21:17:19 -0500

    init

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import datetime
+import urllib.parse as urlparse
+from authentication import api
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = urlparse.urlparse(self.path).query
+        message = urlparse.parse_qs(query)['t'][0]
+        api.update_status(message)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(
+            bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(
+            bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
